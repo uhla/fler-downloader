28ef9083363f9728b1d2bef6b84a6dd48aee3510
Jiri Uhlir <jiri.uhlir@solarwinds.com>
2020-11-18 20:49:37 +0100

    Fetch products with title

--- downloader/app.py
+++ downloader/app.py
@@ -1,6 +1,11 @@
+import base64
+import hashlib
 import sys
-from flask import Flaskgot statis
+from datetime import timezone, datetime
+
+from flask import Flask
 import requests
+import hmac
 
 
 app = Flask(__name__)
@@ -15,22 +20,26 @@ def webpage():
     auth = requests.post("https://www.fler.cz/api/rest/user/auth", json=body, verify=False,
            allow_redirects=False)
 
+    secret_key = auth.json()['secret_key']
+    session_id = auth.json()['session_id']
 
-    secretKey = auth.json()['secret_key']
-    sessionId = auth.json()['session_id']
+    timestamp = str(int(datetime.utcnow().replace(tzinfo=timezone.utc).timestamp()))
+    request_path = "/api/rest/seller/products/list"
+    request_string = "GET\n" + timestamp +"\n" + request_path
+    signature=hmac.new(secret_key.encode(), request_string.encode(), hashlib.sha1).hexdigest()
+    auth_string = "API1_SESS"+" "+session_id+ " "+timestamp+" "+ base64.b64encode(signature.encode()).decode()
 
-    print(secretKey)
-    print(sessionId)
 
-    headers = {"X-FLER-AUTHORIZATION": "123"}
-    productList = requests.get("https://www.fler.cz/api/rest/seller/products/list", headers=headers)
-    print(str(productList.text))
+    headers = {"X-FLER-AUTHORIZATION": auth_string}
+    product_list = requests.get("https://www.fler.cz"+request_path+"?fields=title", headers=headers)
+    print(str(product_list.text))
     return "Here will be some content printed out eventually"
 
 if __name__ == "__main__":
     args = sys.argv[1:]
     username = args[0]
     password = args[1]
-    app.run('0.0.0.0',5002)
+    webpage()
+    # app.run('0.0.0.0',5002)
 
 
