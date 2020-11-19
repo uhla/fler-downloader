04dc7f61dc6a13e7a71bcb71a60146bd9fa198cc
Jiri Uhlir <jiri.uhlir@solarwinds.com>
2020-11-19 11:08:36 +0100

    cleanup dependencies, add safety check for config file

--- downloader/excel_item_reader.py
+++ downloader/excel_item_reader.py
@@ -1,4 +1,5 @@
 import xlrd
+from os import path
 
 from downloader.catalog_item_configuration import CatalogItemConfiguration
 
@@ -7,13 +8,14 @@ class ExcelItemReader:
 
     def read_configuration(self, filename):
         customized_catalog_items = {}
-        wb = xlrd.open_workbook(filename)
-        sheet = wb.sheet_by_index(0)
+        if path.exists(filename):
+            wb = xlrd.open_workbook(filename)
+            sheet = wb.sheet_by_index(0)
 
-        for row_number in range(1, sheet.nrows):
-            if str(sheet.cell_value(row_number, 1)) != '':
-                catalog_item = CatalogItemConfiguration(int(sheet.cell_value(row_number, 1)),
-                                                        sheet.cell_value(row_number, 3))
-                customized_catalog_items[catalog_item.id] = catalog_item
+            for row_number in range(1, sheet.nrows):
+                if str(sheet.cell_value(row_number, 1)) != '':
+                    catalog_item = CatalogItemConfiguration(int(sheet.cell_value(row_number, 1)),
+                                                            sheet.cell_value(row_number, 3))
+                    customized_catalog_items[catalog_item.id] = catalog_item
 
         return customized_catalog_items
